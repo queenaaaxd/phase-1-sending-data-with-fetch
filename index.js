5418d2564efeb98f0456d5a9f22f2be1eb3922ca
+ <+>
2023-03-27 04:38:09 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+// // Add your code here
+function submitData(userName, userEmail) {
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail,
+    }),
+  };
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((response) => response.json())
+    .then((data) => {
+      document.body.innerHTML = data.id;
+    })
+    .catch((error) => {
+      document.body.innerHTML = error.message;
+    });
+}
